b73fa8584c381e6a6a5d2fceac68cf2e6b60f340
stephenhmarsh <stephenforpresident@gmail.com>
2014-04-07 00:33:07 -0400

    second test, name, written and passes

--- people_app/app/models/person.rb
+++ people_app/app/models/person.rb
@@ -2,4 +2,8 @@ class Person < ActiveRecord::Base
 
 	validates(:img_url, :drinks, :license, :first_name, :last_name, presence: true)
 
+	def name
+		return "#{self.first_name} #{self.last_name}"
+	end
+
 end
--- people_app/spec/models/person_spec.rb
+++ people_app/spec/models/person_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Person do
 
 	before(:all) do
-		test_person = Person.create(first_name: "Stephen", last_name: "Marsh", birthdate: "1986-08-12", drinks: 0, img_url: "none", license: true)
+		@test_person = Person.create(first_name: "Stephen", last_name: "Marsh", birthdate: "1986-08-12", drinks: 0, img_url: "none", license: true)
 	end
 
 	# Test to make sure a new Person object has an image_url, a drinks attributes, a license, a first name, a last name when it is created.
@@ -13,8 +13,12 @@ describe Person do
 	it{should validate_presence_of(:first_name)}
 	it{should validate_presence_of(:last_name)}
 
+# #name
+# should return a string that is the object's full name
+
 	describe 'name' do 
 		it "should return a user's full name" do 
+			expect(@test_person.name).to eq("Stephen Marsh")
 		end
 	end
 end
